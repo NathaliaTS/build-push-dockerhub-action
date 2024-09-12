246d45e2f55cdc8656cc99df7f46975ac2ab961a
Nathalia T. Silva <nts.ef@outlook.com>
2024-09-12 16:09:40 -0300

    Add: Lógica simples

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,8 @@
 import * as core from '@actions/core';
-import { exec } from 'child_process';
-import { promisify } from 'util';
-import * as path from 'path';
-import * as fs from 'fs';
-
-const execPromise = promisify(exec);
 
 async function run() {
   try {
-    const dockerImage = core.getInput('docker_image');
-    const dockerfileRepo = core.getInput('dockerfile_repo');
-    const dockerfilePath = core.getInput('dockerfile_path');
-    
-    // Clonar o repositório que contém o Dockerfile
-    const repoName = dockerfileRepo.split('/').pop().replace('.git', '');
-    await execPromise(`git clone ${dockerfileRepo}`);
-    
-    // Construir o Dockerfile
-    const fullDockerfilePath = path.join(repoName, dockerfilePath);
-    if (!fs.existsSync(fullDockerfilePath)) {
-      throw new Error(`Dockerfile not found at path: ${fullDockerfilePath}`);
-    }
-    
-    await execPromise(`docker build -f ${fullDockerfilePath} -t ${dockerImage} ${repoName}`);
-    core.info(`Docker image ${dockerImage} built successfully.`);
-
-    // Fazer pull da imagem do Docker Hub
-    await execPromise(`docker pull ${dockerImage}`);
-    core.info(`Docker image ${dockerImage} pulled successfully.`);
+    core.info('Olá, mundo');
   } catch (error) {
     core.setFailed(`Action failed with error: ${error}`);
   }
